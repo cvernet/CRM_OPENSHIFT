58bbc216b4e61b1aa560102dd81ee8c7cf6162a4
Cyril VERNET <cvernet@gmail.com>
2013-09-15 11:55:26 +0200

    add end

--- server.js
+++ server.js
@@ -144,8 +144,10 @@ var SampleApp = function () {
                     res.write('Data ' + data + '\n');
                });
                
-
+               req.on('end',function(){
                     res.end('end');
+               });
+
 
         };
 
